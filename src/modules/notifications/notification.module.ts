02b667b0f222fdb69cfab5ae433279bd82a3e3d6
erman <ermancanitatli@gmail.com>
2026-04-19 16:05:26 +0300

    feat(notifications): wire history service into module (NOTIF-03)

--- src/modules/notifications/notification.module.ts
+++ src/modules/notifications/notification.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { NotificationService } from './notification.service';
+import { NotificationHistoryService } from './notification-history.service';
+import {
+  NotificationHistory,
+  NotificationHistorySchema,
+} from './schemas/notification-history.schema';
 import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [ConfigModule, UsersModule],
-  providers: [NotificationService],
-  exports: [NotificationService],
+  imports: [
+    ConfigModule,
+    UsersModule,
+    MongooseModule.forFeature([
+      { name: NotificationHistory.name, schema: NotificationHistorySchema },
+    ]),
+  ],
+  providers: [NotificationService, NotificationHistoryService],
+  exports: [NotificationService, NotificationHistoryService],
 })
 export class NotificationModule {}
